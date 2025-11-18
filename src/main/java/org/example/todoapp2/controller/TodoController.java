96b8915d15c39754d1db9c2edf481c1d912f5338
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-18 17:30:18 +0900

    create 로직 작성

--- src/main/java/org/example/todoapp2/controller/TodoController.java
+++ src/main/java/org/example/todoapp2/controller/TodoController.java
@@ -1,7 +1,12 @@
 package org.example.todoapp2.controller;
 
+import org.example.todoapp2.dto.TodoDto;
+import org.example.todoapp2.repository.TodoRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -9,5 +14,24 @@ public class TodoController {
     @GetMapping("/todos")
     public String todos() {
         return "todos";
+    }
+
+    @GetMapping("todos/new")
+    public String newTodo() {
+        return "new";
+    }
+
+    @GetMapping("/todos/create")
+    public String create(
+            @RequestParam String title,
+            @RequestParam String content,
+            Model model
+    ) {
+        TodoDto todoDto = new TodoDto(null, title, content, false);
+        TodoRepository todoRepository = new TodoRepository();
+
+        TodoDto todo = todoRepository.save(todoDto);
+        model.addAttribute("todo", todo);
+        return "create";
     }
 }
--- src/main/java/org/example/todoapp2/repository/TodoRepository.java
+++ src/main/java/org/example/todoapp2/repository/TodoRepository.java
@@ -8,6 +8,13 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class TodoRepository {
+    // storage 라는 Map에(키, 값)을 담은 객체 생성
     private final Map<Long, TodoDto> storage = new ConcurrentHashMap<>();
-    private Long nextId =  1L;
+    private Long nextId =  1L; // 새 Todo를 만들 때 사용할 다음 ID 번호.
+
+    public TodoDto save(TodoDto todo) {
+        todo.setId(nextId++); // 저장 될 떄 마다 새로운 번호 1, 2, 3
+        storage.put(todo.getId(), todo); // 스토리지 객체에 todo id, todo를 넣음
+        return todo; // 다시 todo로 돌아감
+    }
 }
