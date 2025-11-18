953380e6b23c5ab452f258911895c20d5f6506b6
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-18 18:24:15 +0900

    샘플데이터 생성

--- src/main/java/org/example/todoapp2/Todoapp2Application.java
+++ src/main/java/org/example/todoapp2/Todoapp2Application.java
@@ -1,13 +1,25 @@
 package org.example.todoapp2;
 
+import org.example.todoapp2.dto.TodoDto;
+import org.example.todoapp2.repository.TodoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Todoapp2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Todoapp2Application.class, args);
     }
+    @Bean
+    public CommandLineRunner init(TodoRepository todoRepository) {
+        return args -> {
+//            TodoRepository todoRepository = new TodoRepository();
+            todoRepository.save(new TodoDto(null, "study", "JAVA", false));
+            todoRepository.save(new TodoDto(null, "cook", "kimbob", false));
+            todoRepository.save(new TodoDto(null, "workout", "run", false));
 
+        };
+    }
 }
